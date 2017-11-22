fe21148d372c996faebfb444643a2f35094a2b06
Unknown <levsha.elody@gmail.com>
2017-11-22 19:04:29 +0300

    Глава 2 "Unity в действии"

---
+++ glava2/Assets/Scripts/FPSInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSInput : MonoBehaviour {
+	
+	// Update is called once per frame
+
+
+		public float speed = 3f;
+		
+
+
+		// Update is called once per frame
+	void Update () {
+		float deltaX = Input.GetAxis ("Horisontal") * speed;
+		float deltaZ = Input.GetAxis ("Vertical") * speed;
+
+		transform.Translate (deltaX, 0, deltaZ);
+	}
+	
+}
