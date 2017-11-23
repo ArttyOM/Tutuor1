87fbcffaaa7ed84c3693584bfd05929697bb2745
Unknown <levsha.elody@gmail.com>
2017-11-23 19:08:24 +0300

    Добавлено движение

--- glava2/Assets/Scripts/FPSInput.cs
+++ glava2/Assets/Scripts/FPSInput.cs
@@ -1,22 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(tyoeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
 
 public class FPSInput : MonoBehaviour {
 	
-	// Update is called once per frame
-
-
-		public float speed = 3f;
-		
-
-
+	public float speed = 9f;
+	public float gravity = -9.8f;
+	private CharacterController _characterController;
+	void Start ()
+	{
+		_characterController = GetComponent<CharacterController>();
+	}
 		// Update is called once per frame
 	void Update () {
-		float deltaX = Input.GetAxis ("Horisontal") * speed;
-		float deltaZ = Input.GetAxis ("Vertical") * speed;
-
-		transform.Translate (deltaX, 0, deltaZ);
+		float deltaX = Input.GetAxis ("Horizontal") * speed *Time.deltaTime;
+		//Debug.Log (deltaX);
+		float deltaZ = Input.GetAxis("Vertical")*speed* Time.deltaTime;
+		Vector3 movement = Vector3.ClampMagnitude (new Vector3 (deltaX, gravity*Time.deltaTime, deltaZ), speed*Time.deltaTime);
+		movement = transform.TransformDirection (movement);
+		_characterController.Move (movement);
 	}
 	
 }
