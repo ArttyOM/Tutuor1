f6db6872a7d20ba1b981077072acf5768cb7ea87
Unknown <levsha.elody@gmail.com>
2017-11-27 01:05:52 +0300

    Глава 3 "Unity в действии"
    Добавлена стрельба, добавлен 1 враг (пока без ИИ)

---
+++ glava3/Assets/Scripts/FPSInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+
+public class FPSInput : MonoBehaviour {
+	
+	public float speed = 9f;
+	public float gravity = -9.8f;
+	private CharacterController _characterController;
+	void Start ()
+	{
+		_characterController = GetComponent<CharacterController>();
+	}
+		// Update is called once per frame
+	void Update () {
+		float deltaX = Input.GetAxis ("Horizontal") * speed *Time.deltaTime;
+		//Debug.Log (deltaX);
+		float deltaZ = Input.GetAxis("Vertical")*speed* Time.deltaTime;
+		Vector3 movement = Vector3.ClampMagnitude (new Vector3 (deltaX, gravity*Time.deltaTime, deltaZ), speed*Time.deltaTime);
+		movement = transform.TransformDirection (movement);
+		_characterController.Move (movement);
+	}
+	
+}
---
+++ glava3/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+	public float sensitivityHor = 9f;
+	public float sensitivityVert= 9f;
+
+	public float minimumVert = -45f;
+	public float maximumVert = 45f;
+
+	private float _rotationX = 0; //закрытая переменная для угла поворота по вертикали
+
+	public enum RotationAxes
+	{
+		MouseXAndY = 0,
+		MouseX = 1,
+		MouseY = 2
+	}
+
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+
+	void Start()
+	{
+		Rigidbody body = GetComponent<Rigidbody> ();
+		if (body != null)
+			body.freezeRotation = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (axes == RotationAxes.MouseX){
+			transform.Rotate (0, Input.GetAxis("Mouse X")* sensitivityHor, 0);// это поворот в горизонтальной плоскости
+		}
+		else if (axes == RotationAxes.MouseY){
+
+			_rotationX -= Input.GetAxis ("Mouse Y") * sensitivityVert;
+			_rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+			float rotationY = transform.localEulerAngles.y;
+			transform.localEulerAngles = new Vector3 (_rotationX, rotationY, 0);
+			//transform.Rotate (sensitivityVer,0, 0);
+			//Это поворот в вертикальной плоскости
+		}
+		else {
+			_rotationX -= Input.GetAxis ("Mouse Y") * sensitivityVert;
+			_rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+			float delta = Input.GetAxis ("Mouse X") * sensitivityHor;
+			float rotationY = transform.localEulerAngles.y + delta;
+
+			transform.localEulerAngles = new Vector3 (_rotationX, rotationY, 0);
+			//Это комбинированный поворот
+		}
+	}
+
+
+}
