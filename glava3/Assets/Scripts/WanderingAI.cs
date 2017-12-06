0030d24145276086ffb3134dcaa486db32dff06e
Unknown <levsha.elody@gmail.com>
2017-12-07 00:38:18 +0300

    Стрельба ИИ, нанесение урона игроку
    глава 3 закончена

--- glava3/Assets/Scripts/WanderingAI.cs
+++ glava3/Assets/Scripts/WanderingAI.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class WanderingAI : MonoBehaviour {
 
 	private bool _alive;
+
+	[SerializeField] private GameObject fireballPrefab = null;
+	private GameObject _fireball;
+
 	public float speed =3.0f;
 	public float obstacleRange = 5.0f;
 	// Use this for initialization
@@ -21,7 +25,15 @@ public class WanderingAI : MonoBehaviour {
 			RaycastHit hit;
 
 			if (Physics.SphereCast (ray, 0.75f, out hit)) {
-				if (hit.distance < obstacleRange) {
+				GameObject hitObject = hit.transform.gameObject;
+				if (hitObject.GetComponent<PlayerCharacter>()) {
+					if (_fireball == null) {
+						_fireball = Instantiate (fireballPrefab) as GameObject;
+						_fireball.transform.position = transform.TransformPoint (Vector3.forward * 1.5f);
+						_fireball.transform.rotation = transform.rotation;
+					}
+				}
+				else if (hit.distance < obstacleRange) {
 					float angle = Random.Range (-110, 110);
 					transform.Rotate (0, angle, 0);
 				}
